469ec0219ac60f4dae1663e15ae10a70226f0c87
funkyspec <mrgresia@gmail.com>
2017-11-02 11:21:01 -0400

    Change default Laravel welcome text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to ESFC Customer Weekly Ordering
                 </div>
 
                 <div class="links">
