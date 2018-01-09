c14b2193bb465b3a53af0133457323a193902577
funkyspec <mrgresia@gmail.com>
2018-01-09 08:48:57 -0500

    Remove default login/register links from initial views

--- resources/views/welcomeorig.blade.php
+++ resources/views/welcomeorig.blade.php
@@ -66,12 +66,17 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-
+            @if (Route::has('login'))
                 <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
                         <a href="{{ url('/offer/1') }}">This Week's Availability</a>
-
                 </div>
-
+            @endif
 
             <div class="content">
                 <div class="title m-b-md">
