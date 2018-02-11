b950c5769a4e0b726e105529b1b8f467f87827c1
funkyspec <mrgresia@gmail.com>
2018-02-11 16:10:53 -0500

    Fix text error home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,11 +73,11 @@
                 </div>
 
 
-                <!--       <div class="content">
-                <div class="title m-b-md">
+                      <div class="content">
+                            <div class="title m-b-md">
                     Welcome to ESFC Weekly Ordering
                 </div>
-               <p>We will have next week's availability (pickup starting Friday 2/16/18) posted Sunday 2/11/18.</p>  -->
+                <!--  <p>We will have next week's availability (pickup starting Friday 2/16/18) posted Sunday 2/11/18.</p>  -->
 
                 <!--
                 <div class="links">
