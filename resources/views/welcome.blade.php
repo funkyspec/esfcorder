f13bc5678cc751bc889a2e5b248a434a8ead7d7c
funkyspec <mrgresia@gmail.com>
2018-04-29 15:57:16 -0400

    Show weekly availability

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
 
                <div class="top-right links">
-              <!--   <a href="{{ url('/offers/latestoffer') }}">View and Order This Week's Produce</a>  -->
+                <a href="{{ url('/offers/latestoffer') }}">View and Order This Week's Produce</a>
 
                 </div>
 
@@ -77,7 +77,7 @@
                     Welcome to ESFC Weekly Ordering
                 </div>
 
-                <p>We will have next week's availability (pickup starting Friday 5/4/18) posted Sunday 4/29/18.</p>
+             <!--    <p>We will have next week's availability (pickup starting Friday 5/4/18) posted Sunday 4/29/18.</p>  -->
 
                 <!--
                 <div class="links">
